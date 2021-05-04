3c40f02f427f3782673fd9dcde6e8f5521b9aeb6
imneverdied <imneverdied@gmail.com>
2021-05-04 17:58:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -38,13 +38,14 @@ function draw() {
     }
     ctx.stroke();
   }
+
   ctx.fillText("0", 15, 100);
   ctx.fillText("25", 10, 80);
   ctx.fillText("50", 10, 55);
   ctx.fillText("75", 10, 30);
   ctx.fillText("100", 5, 7);
-  currentDateTime = getTime();
-  ctx.fillText(currentDateTime, 250, 10);
+
+  ctx.fillText(moment().format("HH:mm:ss"), 250, 10);
 
   if (AAA === 1) {
     var num = 1;
@@ -56,22 +57,4 @@ function draw() {
   }
   document.getElementById("link").click();
   setTimeout("draw()", 1000);
-}
-
-function paddingLeft(str) {
-  //時間補0
-  if (str.length == 1) return "0" + str;
-  else return str;
-}
-
-function getTime() {
-  //取得時間
-  let today = new Date();
-  let Sec = today.getSeconds().toString();
-  let min = today.getMinutes().toString();
-  let hour = today.getHours().toString();
-  var currentDateTime =
-    paddingLeft(hour) + ":" + paddingLeft(min) + ":" + paddingLeft(Sec);
-
-  return currentDateTime;
 }
